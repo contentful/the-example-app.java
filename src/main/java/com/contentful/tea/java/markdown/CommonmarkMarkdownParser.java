8b80b219c5cd4e3d79d68e16fa2389ae267c376a
Mario Bodemann <mario@contentful.com>
2018-01-09 10:43:29 +0100

    cleanup: simplify markdown parsing method

--- src/main/java/com/contentful/tea/java/markdown/CommonmarkMarkdownParser.java
+++ src/main/java/com/contentful/tea/java/markdown/CommonmarkMarkdownParser.java
@@ -17,6 +17,22 @@ import org.springframework.stereotype.Component;
 public class CommonmarkMarkdownParser implements MarkdownParser {
   private final Parser parser = Parser.builder().build();
 
+  private final AbstractVisitor simplifyInlineMarkdown = new AbstractVisitor() {
+    @Override public void visit(Paragraph paragraph) {
+      super.visit(paragraph);
+
+      final Block parent = paragraph.getParent();
+      if (parent instanceof Document) {
+        if (parent.getFirstChild() == paragraph && parent.getLastChild() == paragraph) {
+          while (paragraph.getFirstChild() != null) {
+            paragraph.insertBefore(paragraph.getFirstChild());
+          }
+          paragraph.unlink();
+        }
+      }
+    }
+  };
+
   private final HtmlRenderer renderer = HtmlRenderer.builder().build();
 
   /**
@@ -26,26 +42,13 @@ public class CommonmarkMarkdownParser implements MarkdownParser {
    * @return a String containing html representation of input, or null on error.
    */
   @Override public String parse(String toParse) {
-    if (toParse != null) {
-      final Node node = parser.parse(toParse);
-      node.accept(new AbstractVisitor() {
-        @Override public void visit(Paragraph paragraph) {
-          super.visit(paragraph);
-          final Block parent = paragraph.getParent();
-          if (parent instanceof Document) {
-            if (parent.getFirstChild() == paragraph && parent.getLastChild() == paragraph) {
-              while(paragraph.getFirstChild() != null) {
-                paragraph.insertBefore(paragraph.getFirstChild());
-              }
-              paragraph.unlink();
-            }
-          }
-        }
-      });
-      final String html = renderer.render(node);
-      return html;
-    } else {
+    if (toParse == null) {
       return null;
     }
+
+    final Node node = parser.parse(toParse);
+    node.accept(simplifyInlineMarkdown);
+
+    return renderer.render(node);
   }
 }
